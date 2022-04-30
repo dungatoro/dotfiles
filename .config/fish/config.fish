ec02e3a1e6f55644a99e09357b3268eae03c17ee
dungatoro <71789541+dungatoro@users.noreply.github.com>
2022-04-30 09:48:41 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,1 @@
 set fish_greeting
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# aliases
-function symble
-    cd school/computing/wordle && python symble.py && cd
-end
